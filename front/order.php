e689a4e8c6acc1c181cd14c96412ea0c3cfaaad7
張建文 <chang180@gmail.com>
2020-07-17 15:04:45 +0800

    20200717 course finished

--- front/order.php
+++ front/order.php
@@ -43,12 +43,23 @@
     $("#movie").on("change", function() {
         getDuration();
     })
-
+    
+    $("#date").on("change",function(){
+        getSession();
+    })
     function getDuration() {
         let id = $("#movie").val();
         $.get("api/get_duration.php",{id},function(duration){
             $("#date").html(duration);
+            getSession();
         })
-        console.log(id);
+    }
+
+    function getSession(){
+        let date=$("#date").val();
+        let id=$("#movie").val();
+        $.get("api/get_session.php",{date,id},function(session){
+            $("#session").html(session);
+        })
     }
 </script>
